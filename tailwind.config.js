d73c5fc1b70be6e06cf26f92eeb5c12484a85a9a
michalkvr <michalkvr.dev@gmail.com>
2023-09-03 21:19:54 +0200

    feat: add all sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero': "url('/assets/images/bg.webp')",
+        'hero': "url('/assets/images/bg.png')",
+      },
+      colors: {
+        'primary': '#1D0306',
+        'secondary': '#9A4E44',
+      },
+      fontFamily: {
+        'prompt': ['MyPrompt', 'sans-serif'],
+      },
+      spacing: {
+        '18': '4.5rem',
+        '22': '5.5rem',
+        '26': '6.5rem',
+        '30': '7.5rem',
+      },
+      fontSize: {
+        'hero': '150px',
+      },
+      rotate: {
+        '360': '360deg',
       }
     },
   },
